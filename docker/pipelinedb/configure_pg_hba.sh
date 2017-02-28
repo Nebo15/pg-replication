e7c8db05402df407ed26e982c6c7d0368ce8fcd8
Andrii Krymets <andrii.krymets@finstar.com>
2017-02-28 15:45:10 +0200

    Add config parametrization via env vars

--- docker/pipelinedb/configure_pg_hba.sh
+++ docker/pipelinedb/configure_pg_hba.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "host    all             pipeline        ${MASTER_HOST}           trust" >> "/scripts/conf/pg_hba.conf"
-echo "host    replication     pipeline        ${REPLICA_HOST}           trust" >> "${PGDATA}/pg_hba.conf"
+echo "host    replication     pipeline        ${REPLICA_HOST}           trust" >> "/scripts/conf/pg_hba.conf"
